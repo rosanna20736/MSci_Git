ff13bb77624bd21bb6d269052f57eb3d6c7939c6
rosanna20736 <r.nichols20736@gmail.com>
2014-10-27 19:44:00 +0000

    200 steps

--- qwalk1.py
+++ qwalk1.py
@@ -15,7 +15,7 @@ q0 = np.matrix('1;0')
 q1 = np.matrix('0;1')
 
 #variables changeable by user
-steps = 1000 #number of steps of the quantum walk to take
+steps = 200 #number of steps of the quantum walk to take
 #initial = q0 #initial coin is |0> - assume inital position is |0>
 initial = 1/np.sqrt(2)*np.matrix('1;1j') #balanced initial coin
 
