36886fae82a61d1d6994560c40883984bc4250bf
rosanna20736 <r.nichols20736@gmail.com>
2014-10-31 00:51:09 +0000

    decoherence animation

--- qwalk1.py
+++ qwalk1.py
@@ -6,9 +6,8 @@ Created on Sun Oct 26 19:44:22 2014
 """
 #import libraries
 import numpy as np
-#import cmath
-#import matplotlib as mpl
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 #qubits |0> and |1>
 q0 = np.matrix('1;0')
@@ -18,7 +17,19 @@ q1 = np.matrix('0;1')
 steps = 50 #number of steps of the quantum walk to take
 #initial = q0 #initial coin is |0> - assume inital position is |0>
 initial = 1/np.sqrt(2)*np.matrix('1;1j') #balanced initial coin
-decoherence = 1 #decoherence rate: probability of a decoherence event occuring per time step
+decoherences = 15 #decoherence rate: probability of a decoherence event occuring per time step
+
+#ANIMATION SET-UP
+#set up figure
+fig = plt.figure()
+ax = plt.axes(xlim=(-steps, steps), ylim=(0, 0.12))
+line, = ax.plot([], [], lw=2)
+# initialization function: plot the background of each frame
+def init():
+    line.set_data([], [])
+    return line,
+#set up x values (even numbers of steps)
+steplabels = range(-steps,steps+2,2)
 
 #set coin flip operator: I (tensor prod) H
 H = 1/np.sqrt(2)*np.matrix('1 1;1 -1') #hadamard matrix
@@ -45,37 +56,60 @@ for x in range(1,2*steps+1):
 
 S = np.kron(S_0,np.outer(q0,q0))+np.kron(S_1,np.outer(q1,q1))
 
-#set initial state of system
-system = np.zeros((4*steps+2,4*steps+2))    #empty density matrix of system: position density matrix (tensor product) coin density matrix
-system[2*steps:2*steps+2,2*steps:2*steps+2] = np.outer(np.conjugate(initial),initial)    #initial state of system (have to explicitly put conjugate into outer product)
-quantum_control=system
-
-#THE QUANTUM WALK
+#THE QUANTUM WALK - WITHOUT NOISE
+quantum_control = np.zeros((4*steps+2,4*steps+2))    #empty density matrix of system: position density matrix (tensor product) coin density matrix
+quantum_control[2*steps:2*steps+2,2*steps:2*steps+2] = np.outer(np.conjugate(initial),initial)    #initial state of system (have to explicitly put conjugate into outer product)
 for x in range(steps):
     quantum_control = S*coin_flip*quantum_control*coin_flip*np.matrix.getH(S) #regular quantum walk - no noise at all
-    system_noiseless = S*coin_flip*system*coin_flip*np.matrix.getH(S) #step of the walk where there is no noise at that step
-    system = (1-decoherence)*system_noiseless   #with probability (1-decoherence), the system is not affected by noise
-    for i in range(2*steps+1):
-         ket_i = np.zeros((2*steps+1,1))
-         ket_i[i] = 1
-         M_i = np.outer(ket_i,ket_i)
-         M_P = np.kron(M_i,I_C)
-         system = system + decoherence*M_P*system_noiseless*np.matrix.getH(M_P) #with probability decoherence, the system's position is measured
-
-#pcolor(system)  #plots system matrix on grid so we can see non-zero values
-
 #Measurement
-p = np.zeros((2*steps+1,1))
 p_quantum_control = np.zeros((2*steps+1,1))
 for i in range(2*steps+1):
     ket_i = np.zeros((2*steps+1,1))
     ket_i[i] = 1
     M_i = np.outer(ket_i,ket_i)
     M_P = np.kron(M_i,I_C)
-    p[i]= np.trace(M_P*np.matrix.getH(M_P)*system)
     p_quantum_control[i]= np.trace(M_P*np.matrix.getH(M_P)*quantum_control)
+    
+#THE QUANTUM WALK - WITH NOISE
+p = np.zeros((2*steps+1,decoherences+1))
+decoherences_list = np.zeros((decoherences+1,1))
+stdevs = np.zeros((decoherences+1,1))
+for j in range(decoherences+1):
+    print(j)
+    decoherence = np.cos((j)*np.pi*0.5/decoherences)
+    decoherences_list[j] =  decoherence
+    #set initial state of system
+    system = np.zeros((4*steps+2,4*steps+2))    #empty density matrix of system: position density matrix (tensor product) coin density matrix
+    system[2*steps:2*steps+2,2*steps:2*steps+2] = np.outer(np.conjugate(initial),initial)    #initial state of system (have to explicitly put conjugate into outer product)
+    #The Quantum Walk
+    for x in range(steps):
+        system_noiseless = S*coin_flip*system*coin_flip*np.matrix.getH(S) #step of the walk where there is no noise at that step
+        system = (1-decoherence)*system_noiseless   #with probability (1-decoherence), the system is not affected by noise
+        for i in range(2*steps+1):
+            ket_i = np.zeros((2*steps+1,1))
+            ket_i[i] = 1
+            M_i = np.outer(ket_i,ket_i)
+            M_P = np.kron(M_i,I_C)
+            system = system + decoherence*M_P*system_noiseless*np.matrix.getH(M_P) #with probability decoherence, the system's position is measured
+    #Measurement
+    for i in range(2*steps+1):
+        ket_i = np.zeros((2*steps+1,1))
+        ket_i[i] = 1
+        M_i = np.outer(ket_i,ket_i)
+        M_P = np.kron(M_i,I_C)
+        p[i,j]= np.trace(M_P*np.matrix.getH(M_P)*system)
+    stdevs[j] = np.std( p[0:2*steps+1:2,j])
+    
+def animate(j):
+    y = p[0:2*steps+1:2,j]
+    line.set_data(steplabels, y)
+    return line,
+
+# call the animator.  blit=True means only re-draw the parts that have changed.
+anim = animation.FuncAnimation(fig, animate, init_func=init, frames=decoherences+1, interval=80, blit=True)
+
+plt.show()
 
 #plot measurement probabilities
-steplabels = range(-steps,steps+2,2)
-plt.plot(steplabels,p[0:2*steps+1:2])
-plt.plot(steplabels,p_quantum_control[0:2*steps+1:2])
+plt.figure()
+plt.plot(decoherences_list,stdevs)
