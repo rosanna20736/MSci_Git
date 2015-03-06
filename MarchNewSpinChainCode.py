06b8849d0dca213d2cec886201a297a14c21588e
mp3911 <michaelprice03@gmail.com>
2015-03-06 11:18:12 +0000

    NewSpinChainTraceDistance

---
+++ MarchNewSpinChainCode.py
@@ -0,0 +1,97 @@
+#NEW QUANTUM WALK/SPIN CHAIN SCRIPT
+##################################
+#PREAMBLE
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import linalg
+from spinchaingenerator import U_spinchain
+##################################
+#INITIAL VARIABLES
+STEPS = 25
+CHAINLENGTH = 5
+results=[]
+J = 0.2
+J1 =0.1
+I_C = np.identity((2))
+chaintype = 0
+##################################
+#SPINCHAIN OPERATOR
+pauli_x = np.matrix(('0 1;1 0'))
+pauli_y = np.matrix(('0 -1j;1j 0'))
+pauli_z = np.matrix(('1 0;0 -1'))
+pauli_i = np.matrix(('1 0;0 1'))
+if chaintype == 1:
+    CHAIN1 = 0
+    CHAIN2 = 0
+    for v in range(CHAINLENGTH-1):
+        COMPONENT1 = 1
+        COMPONENT2 = 1
+        for u in range(CHAINLENGTH):
+            if u==v:
+                COMPONENT1 = np.kron(COMPONENT1,pauli_x)
+                COMPONENT2 = np.kron(COMPONENT2,pauli_y)
+            elif u==v+1:
+                COMPONENT1 = np.kron(COMPONENT1,pauli_x)
+                COMPONENT2 = np.kron(COMPONENT2,pauli_y)
+            else:
+                COMPONENT1 = np.kron(COMPONENT1,pauli_i)
+                COMPONENT2 = np.kron(COMPONENT2,pauli_i)
+    
+        if v==0:
+            CHAIN1 = COMPONENT1 + COMPONENT2
+        else:
+            CHAIN2 = CHAIN2 + COMPONENT1 + COMPONENT2
+    A = (J1*CHAIN1 + J*CHAIN2)
+    A = 1j*A
+if chaintype == 0:
+    A1 = U_spinchain('xy',2)
+    for l in range(CHAINLENGTH-2):
+        A1 = np.kron(A1,I_C)
+    A2 = U_spinchain('xy',CHAINLENGTH-1)
+    A2 = np.kron(I_C,A2)
+    A = 1j*A1*J1 + 1j*A2*J
+if chaintype == 2:
+    A = (np.kron(np.kron(np.kron(np.kron(pauli_x,pauli_x),I_C),I_C),I_C)*J1)+ (np.kron(np.kron(np.kron(np.kron(I_C,pauli_x),pauli_x),I_C),I_C)*J)+ (np.kron(np.kron(np.kron(np.kron(I_C,I_C),pauli_x),pauli_x),I_C)*J)+ (np.kron(np.kron(np.kron(np.kron(I_C,I_C),I_C),pauli_x),pauli_x)*J)+ (np.kron(np.kron(np.kron(np.kron(pauli_y,pauli_y),I_C),I_C),I_C)*J1)+ (np.kron(np.kron(np.kron(np.kron(I_C,pauli_y),pauli_y),I_C),I_C)*J)+ (np.kron(np.kron(np.kron(np.kron(I_C,I_C),pauli_y),pauli_y),I_C)*J)+ (np.kron(np.kron(np.kron(np.kron(I_C,I_C),I_C),pauli_y),pauli_y)*J)
+    A = np.matrix(A)*1j
+
+EXP1 = linalg.expm2(-1*A)
+EXP2 = linalg.expm2(A)
+######################################
+INITIALCOIN = np.matrix([[1],
+                         [0]])
+INITIALCOIN = np.outer(INITIALCOIN,INITIALCOIN)
+INITIALCHAIN = np.matrix([[0],
+                          [1]])
+INITIALCHAIN = np.outer(INITIALCHAIN,INITIALCHAIN)
+A = np.kron(INITIALCOIN,INITIALCHAIN)                                         
+for x in range(CHAINLENGTH-2):
+    A = np.kron(A,INITIALCHAIN)
+SYSTEM = A
+########################################
+#QUANTUM WALK
+ket1 = np.matrix([[1],[0]])
+ket2 = np.matrix([[0],[1]])
+for r in range(STEPS):
+    NEW1 = SYSTEM
+    for x in range(CHAINLENGTH,1,-1):
+        OPP = I_C
+        NEWSYSTEM = NEW1
+        for y in range(x-2):
+            OPP = np.kron(OPP,I_C)
+        OPP1 = np.kron(OPP,ket1)
+        OPP2 = np.kron(OPP,ket2)
+        OPP3 = np.kron(OPP,np.matrix.getH(ket1))
+        OPP4 = np.kron(OPP,np.matrix.getH(ket2))
+        dim = (np.power(2,x-1),np.power(2,x-1))
+        NEW1 = np.zeros(dim)
+        NEW1 = OPP3*NEWSYSTEM*OPP1 + OPP4*NEWSYSTEM*OPP2
+    x = NEW1
+    y = INITIALCHAIN
+    z = linalg.sqrtm(np.conj(x-y)*(x-y))
+    results.append(0.5*np.absolute(np.trace(z)))
+    
+    SYSTEM = np.matrix(EXP1)*np.matrix(SYSTEM)*np.matrix(EXP2)            
+    
+ 
+plt.plot(results)
+
---
+++ SpinChainNonMarkovianity.py
@@ -0,0 +1,194 @@
+#NEW QUANTUM WALK/SPIN CHAIN SCRIPT
+##################################
+#PREAMBLE
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import linalg
+##################################
+#INITIAL VARIABLES
+STEPS = 25
+POSITIONS = 2*STEPS+1
+CHAINLENGTH =5
+results=[]
+results2=[]
+J = 0.2
+J1 =0.1
+I_P = np.identity((POSITIONS))
+I_C = np.identity((2))
+q0 = np.matrix('1;0')
+q1 = np.matrix('0;1')
+    
+##################################
+#SPINCHAIN OPERATOR
+
+pauli_x = np.matrix(('0 1;1 0'))
+pauli_y = np.matrix(('0 -1j;1j 0'))
+pauli_z = np.matrix(('1 0;0 -1'))
+pauli_i = np.matrix(('1 0;0 1'))
+
+CHAIN1 = 0
+CHAIN2 = 0
+for v in range(CHAINLENGTH-1):
+    COMPONENT1 = 1
+    COMPONENT2 = 1
+    for u in range(CHAINLENGTH):
+        if u==v:
+            COMPONENT1 = np.kron(COMPONENT1,pauli_x)
+            COMPONENT2 = np.kron(COMPONENT2,pauli_y)
+        elif u==v+1:
+            COMPONENT1 = np.kron(COMPONENT1,pauli_x)
+            COMPONENT2 = np.kron(COMPONENT2,pauli_y)
+        else:
+            COMPONENT1 = np.kron(COMPONENT1,pauli_i)
+            COMPONENT2 = np.kron(COMPONENT2,pauli_i)
+
+    if v==0:
+        CHAIN1 = COMPONENT1 + COMPONENT2
+    else:
+        CHAIN2 = CHAIN2 + COMPONENT1 + COMPONENT2
+A = (J1*CHAIN1 + J*CHAIN2)
+A = 1j*A
+EXP1 = linalg.expm(-1*A)
+EXP2 = linalg.expm(A)
+######################################
+#INITIAL STATE   
+#INITIALCOIN =               (1/np.sqrt(2))*np.matrix([[1],
+#                                       [1]])
+INITIALCOIN =               np.matrix([[1],
+                                       [0]])
+INITIALCOIN = np.outer(INITIALCOIN,INITIALCOIN)
+#INITIALCHAIN = np.matrix([[0],
+#                         [1]])
+#INITIALCHAIN = np.outer(INITIALCHAIN,INITIALCHAIN)
+INITIALCHAIN = np.matrix([[0,  0],
+                                         [0,  1]])  
+#SYSTEM = np.zeros((4*STEPS+2,4*STEPS+2))
+#SYSTEM[2*STEPS:2*STEPS+2,2*STEPS:2*STEPS+2] = np.outer(np.conjugate(INITIALCOIN),INITIALCOIN)
+#for ad in range(CHAINLENGTH-1):
+#    SYSTEM = np.kron(SYSTEM,INITIALCHAIN)
+A = np.kron(INITIALCOIN,INITIALCHAIN)                                         
+for x in range(CHAINLENGTH-2):
+    A = np.kron(A,INITIALCHAIN)
+SYSTEM = A
+######################################
+#INITIAL STATE
+INITIALCOIN1 =               (1/np.sqrt(2))*np.matrix([[1],
+                                       [-1]])
+INITIALCOIN1 = np.outer(INITIALCOIN1,INITIALCOIN1)
+#INITIALCHAIN = np.matrix([[0],
+#                         [1]])
+#INITIALCHAIN = np.outer(INITIALCHAIN,INITIALCHAIN)
+INITIALCHAIN1 = np.matrix([[0,  0],
+                                         [0,  1]])  
+#SYSTEM = np.zeros((4*STEPS+2,4*STEPS+2))
+#SYSTEM[2*STEPS:2*STEPS+2,2*STEPS:2*STEPS+2] = np.outer(np.conjugate(INITIALCOIN),INITIALCOIN)
+#for ad in range(CHAINLENGTH-1):
+#    SYSTEM = np.kron(SYSTEM,INITIALCHAIN)
+A1 = np.kron(INITIALCOIN1,INITIALCHAIN1)                                         
+for x in range(CHAINLENGTH-2):
+    A1 = np.kron(A1,INITIALCHAIN1)
+
+SYSTEM1 = A1
+########################################
+#QUANTUM WALK
+
+for r in range(STEPS):
+    
+    #Coin Meaurement
+    x = np.zeros((2,2))
+    y = np.zeros((2,2))
+    for i in range(2):
+        ket_i = np.zeros(2)
+        ket_i[i] = 1
+        M_i = np.matrix.getH(ket_i)
+        M_A= np.kron(I_C,M_i)
+        if CHAINLENGTH == 2:
+            x = x + np.matrix(M_A)*SYSTEM*np.matrix.getH(M_A)
+        if CHAINLENGTH > 2:
+            for i1 in range(2):
+                ket_i = np.zeros(2)
+                ket_i[i1] = 1
+                M_i = np.matrix.getH(ket_i)
+                M_B = np.kron(M_A,M_i)
+                if CHAINLENGTH == 3:
+                    x = x + np.matrix(M_B)*SYSTEM*np.matrix.getH(M_B)
+                if CHAINLENGTH > 3:
+                    for i2 in range(2):
+                        ket_i = np.zeros(2)
+                        ket_i[i2] = 1
+                        M_i = np.matrix.getH(ket_i)
+                        M_C = np.kron(M_B,M_i)
+                        if CHAINLENGTH == 4:
+                            x = x + np.matrix(M_C)*SYSTEM*np.matrix.getH(M_C)
+                        if CHAINLENGTH > 4:
+                            for i3 in range(2):
+                                ket_i = np.zeros(2)
+                                ket_i[i3] = 1
+                                M_i = np.matrix.getH(ket_i)
+                                M_D = np.kron(M_C,M_i)
+                                if CHAINLENGTH == 5:
+                                    x = x + np.matrix(M_D)*SYSTEM*np.matrix.getH(M_D)
+                                if CHAINLENGTH > 5:
+                                    for i4 in range(2):
+                                        ket_i = np.zeros(2)
+                                        ket_i[i4] = 1
+                                        M_i = np.matrix.getH(ket_i)
+                                        M_E = np.kron(M_D,M_i)
+                                        if CHAINLENGTH == 6:
+                                            x = x + np.matrix(M_E)*SYSTEM*np.matrix.getH(M_E)
+                                        if CHAINLENGTH > 6:
+                                            for i5 in range(2):
+                                                ket_i = np.zeros(2)
+                                                ket_i[i5] = 1
+                                                M_i = np.matrix.getH(ket_i)
+                                                M_F = np.kron(M_E,M_i)
+                                                if CHAINLENGTH == 7:
+                                                    x = x + np.matrix(M_F)*SYSTEM*np.matrix.getH(M_F)
+                                                if CHAINLENGTH > 7:
+                                                    for i6 in range(2):
+                                                        ket_i = np.zeros(2)
+                                                        ket_i[i6] = 1
+                                                        M_i = np.matrix.getH(ket_i)
+                                                        M_G = np.kron(M_F,M_i)
+                                                        if CHAINLENGTH == 8:
+                                                            x = x + np.matrix(M_G)*SYSTEM*np.matrix.getH(M_G)
+                                                        if CHAINLENGTH > 8:
+                                                            for i7 in range(2):
+                                                                ket_i = np.zeros(2)
+                                                                ket_i[i7] = 1
+                                                                M_i = np.matrix.getH(ket_i)
+                                                                M_H = np.kron(M_G,M_i)
+                                                                if CHAINLENGTH == 9:
+                                                                    x = x + np.matrix(M_H)*SYSTEM*np.matrix.getH(M_H)
+                                                                if CHAINLENGTH > 9:
+                                                                    for i8 in range(2):
+                                                                        ket_i = np.zeros(2)
+                                                                        ket_i[i8] = 1
+                                                                        M_i = np.matrix.getH(ket_i)
+                                                                        M_I = np.kron(M_H,M_i)
+                                                                        if CHAINLENGTH == 10:
+                                                                            x = x + np.matrix(M_I)*SYSTEM*np.matrix.getH(M_I)
+                                                                        if CHAINLENGTH > 10:
+                                                                            for i9 in range(2):
+                                                                                ket_i = np.zeros(2)
+                                                                                ket_i[i9] = 1
+                                                                                M_i = np.matrix.getH(ket_i)
+                                                                                M_J = np.kron(M_I,M_i)
+                                                                                if CHAINLENGTH == 11:
+                                                                                    x = x + np.matrix(M_J)*SYSTEM*np.matrix.getH(M_J)           
+               
+#                x = x + np.matrix(M_)*SYSTEM*np.matrix.getH(M_)
+#                y = y + np.matrix(M_C)*SYSTEM1*np.matrix.getH(M_C)
+    y = INITIALCHAIN
+    z = linalg.sqrtm(np.conj(x-y)*(x-y))
+    results.append(0.5*np.absolute(np.trace(z)))
+#    z = np.conj(x-y)*(x-y)
+    
+    SYSTEM = np.matrix(EXP1)*np.matrix(SYSTEM)*np.matrix(EXP2) 
+#
+
+#    SYSTEM1 = np.matrix(EXP1)*np.matrix(SYSTEM1)*np.matrix(EXP2)           
+    
+ 
+plt.plot(results)
+
